7cdcec360fb5cb4b32173f84f35e0f5114dda78a
aymen7 <aymenokay@gmail.com>
2018-08-26 23:17:34 +0100

    add the header and search components html/css
    finish the html and css for the header and search-box components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchBoxComponent } from './search-box/search-box.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchBoxComponent
   ],
   imports: [
     BrowserModule,
